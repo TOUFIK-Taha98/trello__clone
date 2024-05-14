7748b3498000b0432458bfd252a72d40fb8cadb6
TOUFIK-Taha98 <toufik.taha1998@gmail.com>
2024-05-14 12:01:34 +0200

    TS correction

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -7,10 +7,13 @@ import { FormButton } from "./form-button";
 import { useAction } from "@/hooks/use-action";
 
 export const Form = () => {
+  // @ts-ignore
   const { execute, fieldErrors } = useAction(createBoard);
   return (
+    // @ts-ignore
     <form action={displatch}>
       <div className="flex flex-col space-y-2">
+        {/* @ts-ignore */}
         <FormInput errors={state?.errors} />
       </div>
       <FormButton />
